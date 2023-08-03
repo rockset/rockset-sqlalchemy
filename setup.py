5ac494deb82bcb554826a4c77bff0f9cc5e74186
Aarav Borthakur <aarav@rockset.com>
2023-08-03 16:03:51 -0700

    Set version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="rockset-sqlalchemy",
-    version="0.0.1",
+    version="1.0.0",
     author="Rockset",
     author_email="support@rockset.com",
     keywords=["Rockset", "rockset-client"],
