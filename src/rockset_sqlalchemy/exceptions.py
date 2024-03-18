db37a2f904146f04a6edfe48f3101291b8c06c77
Jonathan Kula <jonathan@rockset.com>
2024-03-18 10:39:13 -0700

    format with black

--- src/rockset_sqlalchemy/exceptions.py
+++ src/rockset_sqlalchemy/exceptions.py
@@ -1,31 +1,27 @@
 import rockset
 from json import loads
 
+
 class Error(rockset.exceptions.RocksetException):
     @classmethod
     def map_rockset_exception(cls, exc):
         err_body = loads(exc.body)
-        args = [
-            err_body["message"],
-            exc.status,
-            err_body["type"]
-        ]
+        args = [err_body["message"], exc.status, err_body["type"]]
         exc_type = type(exc)
         if (
-            exc_type == rockset.exceptions.ApiTypeError or
-            exc_type == rockset.exceptions.ApiValueError or
-            exc_type == rockset.exceptions.ApiAttributeError or
-            exc_type == rockset.exceptions.ApiKeyError or 
-            exc_type == rockset.exceptions.NotFoundException or
-            exc_type == rockset.exceptions.InputException or  
-            exc_type == rockset.exceptions.InitializationException or
-            exc_type == rockset.exceptions.BadRequestException
-
+            exc_type == rockset.exceptions.ApiTypeError
+            or exc_type == rockset.exceptions.ApiValueError
+            or exc_type == rockset.exceptions.ApiAttributeError
+            or exc_type == rockset.exceptions.ApiKeyError
+            or exc_type == rockset.exceptions.NotFoundException
+            or exc_type == rockset.exceptions.InputException
+            or exc_type == rockset.exceptions.InitializationException
+            or exc_type == rockset.exceptions.BadRequestException
         ):
             ret = ProgrammingError(*args)
         elif (
-            exc_type == rockset.exceptions.UnauthorizedException or 
-            exc_type == rockset.exceptions.ForbiddenException
+            exc_type == rockset.exceptions.UnauthorizedException
+            or exc_type == rockset.exceptions.ForbiddenException
         ):
             ret = OperationalError(*args)
         elif exc_type == rockset.exceptions.ServiceException:
